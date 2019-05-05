3cc1216797de28538555e0968e1482bf9fb9c0eb
Lucas Bastos <lucas.bastos@interakt.com.br>
2019-05-05 20:59:33 -0300

    Resync

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
-import { GraphQLServer } from "graphql-yoga";
-import db from "./db";
-import Query from "./resolvers/Query";
-import Mutation from "./resolvers/Mutation";
-import User from "./resolvers/User";
-import Post from "./resolvers/Post";
-import Comment from "./resolvers/Comment";
+import { GraphQLServer, PubSub } from 'graphql-yoga'
+import db from './db'
+import Query from './resolvers/Query'
+import Mutation from './resolvers/Mutation'
+import Subscription from './resolvers/Subscription'
+import User from './resolvers/User'
+import Post from './resolvers/Post'
+import Comment from './resolvers/Comment'
+
+const pubsub = new PubSub()
 
 const server = new GraphQLServer({
-  typeDefs: "./src/schema.graphql",
-  resolvers: {
-    Query,
-    Mutation,
-    User,
-    Post,
-    Comment
-  },
-  context: {
-    db
-  }
-});
+    typeDefs: './src/schema.graphql',
+    resolvers: {
+        Query,
+        Mutation,
+        Subscription,
+        User,
+        Post,
+        Comment
+    },
+    context: {
+        db,
+        pubsub
+    }
+})
 
 server.start(() => {
-  console.log("The server is up!");
-});
+    console.log('The server is up!')
+})
