e33b37e2653482d2f16dfd26beeda39dcba02aee
Lucas Bastos <lucas.bastos@interakt.com.br>
2019-05-05 11:08:23 -0300

    Refactor using Input Type

--- src/index.js
+++ src/index.js
@@ -85,9 +85,28 @@ const typeDefs = `
     }
 
     type Mutation {
-        createUser(name: String!, email: String!, age: Int): User!
-        createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
-        createComment(text: String!, author: ID!, post: ID!): Comment!
+        createUser(data: CreateUserInput): User!
+        createPost(data: CreatePostInput): Post!
+        createComment(data: CreateCommentInput): Comment!
+    }
+
+    input CreateUserInput {
+      name: String!,
+      email: String!,
+      age: Int
+    }
+
+    input CreatePostInput {
+      title: String!,
+      body: String!,
+      published: Boolean!,
+      author: ID!
+    }
+
+    input CreateCommentInput {
+      text: String!,
+      author: ID!,
+      post: ID!
     }
 
     type User {
@@ -164,7 +183,7 @@ const resolvers = {
   },
   Mutation: {
     createUser(parent, args, ctx, info) {
-      const emailTaken = users.some(user => user.email === args.email);
+      const emailTaken = users.some(user => user.email === args.data.email);
 
       if (emailTaken) {
         throw new Error("Email taken");
@@ -172,7 +191,7 @@ const resolvers = {
 
       const user = {
         id: uuidv4(),
-        ...args
+        ...args.data
       };
 
       users.push(user);
@@ -180,7 +199,7 @@ const resolvers = {
       return user;
     },
     createPost(parent, args, ctx, info) {
-      const userExists = users.some(user => user.id === args.author);
+      const userExists = users.some(user => user.id === args.data.author);
 
       if (!userExists) {
         throw new Error("User not found");
@@ -188,17 +207,17 @@ const resolvers = {
 
       const post = {
         id: uuidv4(),
-        ...args
+        ...args.data
       };
 
-      posts.push(push);
+      posts.push(post);
 
       return post;
     },
     createComment(parent, args, ctx, info) {
-      const userExists = users.some(user => user.id === args.author);
+      const userExists = users.some(user => user.id === args.data.author);
       const postExists = posts.some(
-        post => post.id === args.post && post.published
+        post => post.id === args.data.post && post.published
       );
 
       if (!userExists || !postExists) {
@@ -207,7 +226,7 @@ const resolvers = {
 
       const comment = {
         id: uuidv4(),
-        ...args
+        ...args.data
       };
 
       comments.push(comment);
