62d1ecc412b91bf9d558ab2e22fdbfd719b2eeda
Lucas Bastos <lucas.bastos@interakt.com.br>
2019-05-05 17:59:13 -0300

    Add update Post

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -95,6 +95,28 @@ const Mutation = {
 
     return deletedPosts[0];
   },
+  updatePost(parent, { id, data }, { db }, info) {
+    const post = db.posts.find(post => post.id === id);
+    console.log(post);
+
+    if (!post) {
+      throw new Error("Post not found");
+    }
+
+    if (typeof data.title === "string") {
+      post.title = data.title;
+    }
+
+    if (typeof data.body === "string") {
+      post.body = data.body;
+    }
+
+    if (typeof data.published === "boolean") {
+      post.published = data.published;
+    }
+
+    return post;
+  },
   createComment(parent, args, { db }, info) {
     const userExists = db.users.some(user => user.id === args.data.author);
     const postExists = db.posts.some(
